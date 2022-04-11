de260c17a2fd5bcb929c4785f85c2a7f1e54e2d4
Bolanle <bola0321@gmail.com>
2022-04-11 18:19:19 -0400

    added letterize, firstthingfirst, reverse

--- src/StringLab.java
+++ src/StringLab.java
@@ -5,14 +5,44 @@ public class StringLab {
 		// TODO Auto-generated method stub
 		System.out.println(capitalize("e"));
 	}
-
+/*********************capitalize*******************
+ * capitalizes first letter of String passed to method, 
+ * lowercases the other lettters
+ * */
 	public static String capitalize(String word) {
 		if(word == null) {
 			return "";
 		}
 		String substrCap = word.substring(0, 1).toUpperCase() ;
 		String substrLow = word.substring(1).toLowerCase();
-		return substrCap + substrLow; 
+		return substrCap + substrLow; 	
+	}
+	
+	//public static String wheresWaldo (String phrase) {
 		
+	//}
+	
+	public static void firstThingsFirst (String a, String b) {
+		char firstWordVal = a.charAt(0);
+		char secondWordVal = b.charAt(0);
+		int compareAB = Character.compare(firstWordVal, secondWordVal);
+		
+		if (compareAB >= 0) {
+			System.out.println(a +" " + b );
+		}else if (compareAB <0) {
+			System.out.println(b + " " + a);
+		}		
+	}
+	
+	public static void reverse (String s) {
+		for (int i = s.length()-1 ; i>=0; i--) {
+			System.out.println(s.charAt(i));
+		}
+	}
+	
+	public static void letterize(String word) {
+		for (int i = 0; i<= word.length(); i++) {
+			System.out.println(word.charAt(i));
+		}
 	}
 }
